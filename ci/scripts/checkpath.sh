7f8439f15da0a258e5a3a764f3d8c5eded1f352e
Anton Ustyuzhanin <au@hellofresh.com>
2019-09-24 16:28:12 +0200

    Add pipeline

--- ci/scripts/checkpath.sh
+++ ci/scripts/checkpath.sh
@@ -2,18 +2,6 @@
 
 set -ex
 
-temp() {
-    apk --update --quiet --no-progress add git jq
-
-    IGNORED_PATHS="$(echo "$IGNORED_PATHS" | jq -r '":!" + (. // [])[]')"
-    test -z "$IGNORED_PATHS" && exit 0
-
-    HEAD=$(jq -r '.[] | select(.name=="head_sha") .value' .git/resource/metadata.json)
-    BASE=$(jq -r '.[] | select(.name=="base_sha") .value' .git/resource/metadata.json)
-
-    git diff --name-only "${BASE}~1..${HEAD}" -- ${IGNORED_PATHS} >../changed-files/files.txt
-}
-
 _checkpath_is_pkg_installed() {
     command -v "$1" >/dev/null
 }
@@ -51,11 +39,20 @@ _checkpath_prepare() {
     _checkpath_is_pkg_installed jq || _checkpath_install_pkg "$pkgman" jq
 }
 
+_checkpath_maybe_teliaoss_pr() {
+    metadata_file=$(find . -path '*.git/resource/metadata.json')
+    test -z "$metadata_file" && return
+    HEAD=$(jq -r '.[] | select(.name=="head_sha") .value' .git/resource/metadata.json)
+    BASE=$(jq -r '.[] | select(.name=="base_sha") .value' .git/resource/metadata.json)
+    git diff --name-only "${BASE}~1..${HEAD}"
+}
+
 checkpaths() {
-    test -z "$CHECKPATHS" && exit 0
+    test -z "$CHECKPATH" && exit 0
     _checkpath_prepare
-    diff_paths=$(git diff --name-only HEAD~1..HEAD)
+    diff_paths=$(_checkpath_maybe_teliaoss_pr)
     echo "$diff_paths"
 }
 
+CHECKPATH="path1"
 checkpaths
